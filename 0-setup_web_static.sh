38c88b8c07b7959e1a0c967e49af91aaaf8bec52
Grace-ngigi <gracengigi859@gmail.com>
2023-09-07 19:24:36 -0700

    preparing web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,43 +1,49 @@
 #!/usr/bin/env bash
+# Bash script that sets up webservers for deployment of web_static
 
-# Install Nginx if it's not already installed
+# Check if nginx is installed else install
 if ! command -v nginx &> /dev/null; then
     sudo apt-get update
-    sudo apt-get -y install nginx
+    sudo apt-get install -y nginx
     sudo service nginx start
 fi
 
-# Create the directories and give ownership
+# Create the necessary folders and files if they don't exist
 sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared
-
-# Create a fake HTML file
+sudo mkdir -p /data/web_static/shared/
 echo "<!DOCTYPE html>
 <html>
-<head>
-<title>Deploying web static</title>
-<head>
-<body>
-<h1>Deploying to two servers.</h1>
-</body>" | sudo tee /data/web_static/releases/test/index.html
+  <head>
+   <title>Test page for nginx</title>
+  </head>
+  <body>
+   <h1>My Cat's name is Loki.</h1>
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-# Create or recreate the symbolic link
+# Create a symbolic link and delete it if it exists
 sudo rm -f /data/web_static/current
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# change ownership
+# Give ownership of /data/ folder to ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
 
-# Update Nginx configuration
-nginx_config="/etc/nginx/sites-available/default"
+# Update nginx configurations using an alias
 echo "server {
     listen 80 default_server;
     server_name _;
+
     location /hbnb_static {
         alias /data/web_static/current/;
-    } 
+    }
+
 }" | sudo tee /etc/nginx/sites-available/default
-# Restart Nginx
+
+# Test nginx configurations
+sudo nginx -t
+
+# Restart nginx to apply changes
 sudo service nginx restart
-# exit
-exit
+
+# Exit the script successfully
+exit 0
