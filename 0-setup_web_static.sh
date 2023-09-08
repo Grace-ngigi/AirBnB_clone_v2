d8c8c86a2b3f2586ad57406ae735d2426392d131
Grace-ngigi <gracengigi859@gmail.com>
2023-09-07 18:40:26 -0700

    preparing servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,15 +4,24 @@
 if ! command -v nginx &> /dev/null; then
     sudo apt-get update
     sudo apt-get -y install nginx
+    sudo service nginx start
 fi
 
-# Create the necessary directories
-sudo mkdir -p /data/web_static/{releases/test,shared}
+# Create the directories and give ownership
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/wen_tatic/shared
 sudo touch /data/web_static/releases/test/index.html
 sudo chown -R ubuntu:ubuntu /data/
 
 # Create a fake HTML file
-echo "This is a test HTML file." | sudo tee /data/web_static/releases/test/index.html > /dev/null
+echo "<!DOCTYPE html>
+<html>
+<head>
+<title>Deploying web static</title>
+<head>
+<body>
+<h1>Deploying to two servers.</h1>
+</body>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
 # Create or recreate the symbolic link
 sudo rm -f /data/web_static/current
@@ -21,17 +30,11 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 # Update Nginx configuration
 nginx_config="/etc/nginx/sites-available/default"
 echo "server {
-    listen 80;
+    listen 80 defult_server;
     server_name _;
     location /hbnb_static {
         alias /data/web_static/current/;
-        index index.html;
-    }
-    location / {
-        proxy_pass http://localhost:5000; # Adjust this line if needed
-    }
-}" | sudo tee "$nginx_config" > /dev/null
-
+    } 
+}" | sudo tee "$nginx_config"
 # Restart Nginx
 sudo service nginx restart
-
