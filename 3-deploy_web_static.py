bec9d9280c650f30c1397d3e9863cc4f087f2e1d
Grace-ngigi <gracengigi859@gmail.com>
2023-09-10 19:52:26 -0700

    full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -9,15 +9,66 @@ env.user = 'ubuntu'
 
 
 def do_pack():
-    # Your do_pack code as provided in 1-pack_web_static.py
+    """
+    Generates a .tgz archive from the contents of the web_static folder.
+    """
+    # Get the current timestamp for the archive name
+    now = datetime.utcnow()
+    archive_name = "versions/web_static_{}{}{}{}{}{}.tgz".format(now.year,
+                                                                 now.month,
+                                                                 now.day,
+                                                                 now.hour,
+                                                                 now.minute,
+                                                                 now.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(archive_name)).failed is True:
+        return None
+    return archive_name
 
-    def do_deploy(archive_path):
-        # Your do_deploy code as provided in 2-deploy_web_static.py
 
-        def deploy():
-            """
-            Create and distribute an archive to web servers.
-            """
+def do_deploy(archive_path):
+    """
+    Distribute an archive to web servers and deploy it.
+    """
+    if not os.path.exists(archive_path):
+        return False
+
+    try:
+        # Upload the archive to /tmp/ directory on the web server
+        put(archive_path, '/tmp/')
+
+        # Get the archive filename without extension
+        archive_filename = os.path.basename(archive_path)
+        archive_name_noext = os.path.splitext(archive_filename)[0]
+
+        # Create the directory structure for deployment
+        release_folder = '/data/web_static/releases/' + archive_name_noext
+        run('mkdir -p {}'.format(release_folder))
+
+        # Uncompress the archive to the release folder
+        run('tar -xzf /tmp/{} -C {}'.format(archive_filename, release_folder))
+
+        # Delete the archive from the web server
+        run('rm /tmp/{}'.format(archive_filename))
+
+        # Create a new symbolic link
+        current_link = '/data/web_static/current'
+        run('rm -f {}'.format(current_link))
+        run('ln -s {} {}'.format(release_folder, current_link))
+
+        print('New version deployed!')
+        return True
+    except Exception as e:
+        print('Deployment failed:', e)
+        return False
+
+
+def deploy():
+    """
+    Create and distribute an archive to web servers.
+    """
     # Create the archive
     archive_path = do_pack()
 
